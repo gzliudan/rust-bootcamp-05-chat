0a33909be7a36146ed9c748b8da419b45b10f1d3
Daniel Liu <liudaniel@qq.com>
2024-06-15 13:07:16 +0800

    refactor: improve test code

--- chat_server/src/models/user.rs
+++ chat_server/src/models/user.rs
@@ -165,10 +165,8 @@ impl SigninUser {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::AppConfig;
+    use crate::test_util::get_test_pool;
     use anyhow::Result;
-    use sqlx_db_tester::TestPg;
-    use std::path::Path;
 
     #[test]
     fn hash_password_and_verify_should_work() -> Result<()> {
@@ -181,12 +179,9 @@ mod tests {
 
     #[tokio::test]
     async fn create_duplicate_user_should_fail() -> Result<()> {
-        let config = AppConfig::load()?;
-        let tdb = TestPg::new(config.server.test_db_url, Path::new("../migrations"));
-        let pool = tdb.get_pool().await;
+        let (_tdb, pool) = get_test_pool(None).await;
 
-        let input = CreateUser::new("none", "Tyr Chen", "tchen@acme.org", "hunter42");
-        User::create(&input, &pool).await?;
+        let input = CreateUser::new("acme", "Tyr Chen", "tchen@acme.org", "hunter42");
         let ret = User::create(&input, &pool).await;
         match ret {
             Err(AppError::EmailAlreadyExists(email)) => {
@@ -199,11 +194,8 @@ mod tests {
 
     #[tokio::test]
     async fn create_and_verify_user_should_work() -> Result<()> {
-        let config = AppConfig::load()?;
-        let tdb = TestPg::new(config.server.test_db_url, Path::new("../migrations"));
-        let pool = tdb.get_pool().await;
-
-        let input = CreateUser::new("none", "Tyr Chen", "tchen@acme.org", "hunter42");
+        let (_tdb, pool) = get_test_pool(None).await;
+        let input = CreateUser::new("none", "Tian Chen", "tyr@acme.org", "hunter42");
         let user = User::create(&input, &pool).await?;
         assert_eq!(user.email, input.email);
         assert_eq!(user.fullname, input.fullname);
